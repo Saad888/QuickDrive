fa238896b5280b774496b02059b6bd17b982d866
Saad Taimoor <saadtaimoor8@gmail.com>
2021-05-04 14:26:36 -0400

    Initial-Commit

---
+++ QuickDrive/QuickDrive/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using QuickDrive.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QuickDrive.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
